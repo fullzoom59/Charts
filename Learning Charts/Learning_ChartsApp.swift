61736725bf3842480a801ccc63e16cabfb5bdd81
Vlad <119012478+fullzoom59@users.noreply.github.com>
2024-05-13 19:47:52 +0200

    Setup Project

---
+++ Learning Charts/Learning_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_ChartsApp.swift
+//  Learning Charts
+//
+//  Created by fullzoom on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Learning_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
